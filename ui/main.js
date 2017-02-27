16537e604cf2758aa30dd6a8700d2ce4a2defe94
pvnprb <pavanprb@gmail.com>
2017-02-27 23:29:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,11 +8,10 @@ var img=document.getElementById('madi');
 var marginLeft=0;
 
 function moveRight() {
-    marginLeft=marginLeft+10;
+    marginLeft=marginLeft+5;
     img.style.marginLeft=marginLeft+'px';
 }
 
 img.onclick = function() { 
-   var interval=setInterval(moveRight,100);
-   // img.style.marginLeft='100px';
+   var interval=setInterval(moveRight,50);
 };
