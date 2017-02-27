ba71622a62dbab5fb048154e94b0a62e273eb9dd
pvnprb <pavanprb@gmail.com>
2017-02-27 23:16:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,8 @@ console.log('Loaded!');
 
 var element=document.getElementById('main-text');
 element.innerHTML='New Value Of Text';
+
+var img=document.getElementById('madi');
+img.onclick=fucntion(){
+    img.style.marginLeft='100px';
+};
